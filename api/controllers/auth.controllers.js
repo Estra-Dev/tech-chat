fa66548943d2698447492f116bdb108e21e56bb3
Estra-Dev <dominikolyson@gmail.com>
2024-05-03 10:43:36 +0100

    complete login functionality

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,20 +1,23 @@
 import { User } from "../model/user.model.js";
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const salt = 10;
 
 // signup functionality
 export const signUp = async (req, res, next) => {
-  const { username, email, password } = req.body;
+  const { username, email, password, reenterpassword } = req.body;
 
   if (
     !username ||
     !password ||
     !email ||
+    !reenterpassword ||
     username === "" ||
     password === "" ||
-    email === ""
+    email === "" ||
+    reenterpassword === ""
   ) {
     return next(errorHandler(400, "all fields are required"));
   }
@@ -41,19 +44,25 @@ export const signUp = async (req, res, next) => {
     );
   }
   try {
-    const existingUser = await User.findOne({ username });
-    if (existingUser) {
+    const existingUserName = await User.findOne({ username });
+    const existingEmail = await User.findOne({ email });
+    if (existingUserName) {
       return next(errorHandler(400, "user already exist"));
     }
+    if (existingEmail) {
+      return next(errorHandler(400, "email already use"));
+    }
 
-    const hashedPassword = await bcryptjs.hash(password, salt);
+    if (reenterpassword === password) {
+      const hashedPassword = await bcryptjs.hash(password, salt);
+    }
     const newUser = User.create({
       username,
       email,
       password: hashedPassword,
     });
 
-    res.status(201).json("register successfully");
+    res.status(201).json("registered successfully");
     console.log(newUser);
   } catch (error) {
     console.log(error);
@@ -63,8 +72,36 @@ export const signUp = async (req, res, next) => {
 // login functionality
 
 export const login = async (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (!username || !password || username === "" || password === "") {
+    return next(errorHandler(400, "all fields are required"));
+  }
+
   try {
+    const validUser = await User.findOne({ username });
+
+    if (!validUser) {
+      return next(errorHandler(400, "this user does not exist"));
+    }
+
+    const passOk = await bcryptjs.compare(password, validUser.password);
+
+    if (!passOk) {
+      return next(errorHandler(403, "worng credential, check login details"));
+    } else {
+      const { password, ...rest } = validUser._doc;
+
+      const token = jwt.sign({ id: validUser._id }, process.env.SECRETE);
+      res
+        .status(200)
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .json(rest);
+      console.log(rest);
+    }
   } catch (error) {
-    console.log(error);
+    next(error);
   }
 };
